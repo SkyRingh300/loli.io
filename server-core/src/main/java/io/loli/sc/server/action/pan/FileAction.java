ad538f75cc45b57409ae078c1d921fa94f5b1f2b
chocotan <loli@linux.com>
2014-08-23 02:32:52 +0800

    Change tab to space

--- server-core/src/main/java/io/loli/sc/server/action/pan/FileAction.java
+++ server-core/src/main/java/io/loli/sc/server/action/pan/FileAction.java
@@ -18,43 +18,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Named
 public class FileAction {
 
-	@Inject
-	private FolderService fs;
+    @Inject
+    private FolderService fs;
 
-	@RequestMapping("list")
-	public String list(
-			@RequestParam(value = "pid", required = false) Integer pid,
-			HttpServletRequest request) {
-		if (pid == null) {
-			pid = 0;
-		}
-		User user = (User) request.getSession().getAttribute("user");
-		List<FolderEntity> folders = fs.listByUserAndPath(user, pid);
-		request.setAttribute("folderList", folders);
-		request.setAttribute("rootFolder", fs.findRootByUser(user));
-		if (pid != 0) {
-			FolderEntity parent = fs.findById(pid);
-			request.setAttribute("parent", parent);
-		}
-		return "pan/fileList";
+    @RequestMapping("list")
+    public String list(@RequestParam(value = "pid", required = false) Integer pid, HttpServletRequest request) {
+        if (pid == null) {
+            pid = 0;
+        }
+        User user = (User) request.getSession().getAttribute("user");
+        List<FolderEntity> folders = fs.listByUserAndPath(user, pid);
+        request.setAttribute("folderList", folders);
+        request.setAttribute("rootFolder", fs.findRootByUser(user));
+        if (pid != 0) {
+            FolderEntity parent = fs.findById(pid);
+            request.setAttribute("parent", parent);
+        }
+        return "pan/fileList";
 
-	}
+    }
 
-	@RequestMapping("add")
-	public String add(@RequestParam(value = "pid") int parentId,
-			@RequestParam(value = "name") String folderName,
-			HttpServletRequest request) {
-		FolderEntity parent = fs.findById(parentId);
-		FolderEntity fe = new FolderEntity();
-		fe.setParent(parent);
-		fe.setName(folderName);
-		fe.setFullPath(parent.getFullPath() + folderName + "/");
-		fe.setCreateDate(new Date());
+    @RequestMapping("add")
+    public String add(@RequestParam(value = "pid") int parentId, @RequestParam(value = "name") String folderName,
+        HttpServletRequest request) {
+        FolderEntity parent = fs.findById(parentId);
+        FolderEntity fe = new FolderEntity();
+        fe.setParent(parent);
+        fe.setName(folderName);
+        fe.setFullPath(parent.getFullPath() + folderName + "/");
+        fe.setCreateDate(new Date());
 
-		User user = (User) request.getSession().getAttribute("user");
+        User user = (User) request.getSession().getAttribute("user");
 
-		fe.setUser(user);
-		fs.save(fe);
-		return this.list(parentId, request);
-	}
+        fe.setUser(user);
+        fs.save(fe);
+        return this.list(parentId, request);
+    }
 }
