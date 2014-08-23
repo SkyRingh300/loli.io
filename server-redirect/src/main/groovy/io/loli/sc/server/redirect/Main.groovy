cd9f1c93260ad7e70bd2bf0f135e4267a1d34419
chocotan <loli@linux.com>
2014-08-24 01:45:06 +0800

    Add groovy main script

---
+++ server-redirect/src/main/groovy/io/loli/sc/server/redirect/Main.groovy
@@ -0,0 +1,8 @@
+package io.loli.sc.server.redirect
+
+import io.loli.sc.server.redirect.config.Config;
+import io.loli.sc.server.redirect.socket.RedirectFilter;
+import io.loli.storage.redirect.RedirectServer;
+
+server = new RedirectServer().filter(new RedirectFilter()).port(Config.port);
+server.start();
