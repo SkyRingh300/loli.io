912a33db6f1c0cc54d965411c7ff57f7fc19b4e0
chocotan <loli@linux.com>
2014-08-24 21:22:10 +0800

    Remove blank lines

--- server-script/src/main/groovy/image_redirect_server.groovy
+++ server-script/src/main/groovy/image_redirect_server.groovy
@@ -1,8 +1,6 @@
-
-
 import io.loli.sc.server.redirect.config.Config;
 import io.loli.sc.server.redirect.socket.RedirectFilter;
 import io.loli.storage.redirect.RedirectServer;
 
 server = new RedirectServer().filter(new RedirectFilter()).port(Config.port);
-server.start();
+server.start;
