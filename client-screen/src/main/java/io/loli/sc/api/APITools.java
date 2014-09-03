c51cb21497956a5dc9539ca9bfb0971b2f92fd1a
chocotan <loli@linux.com>
2014-09-03 22:17:32 +0800

    SCREEN-7 Upload now support https 还需要测试，且在部分系统里有问题

--- client-screen/src/main/java/io/loli/sc/api/APITools.java
+++ client-screen/src/main/java/io/loli/sc/api/APITools.java
@@ -1,24 +1,47 @@
 package io.loli.sc.api;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
-import java.io.IOException;
-import java.util.List;
 
 public class APITools {
     /**
      * 发出post请求
      */
     public String post(String postUrl, List<NameValuePair> params) {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+        SSLContext sslContext = null;
+        try {
+            sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+                // 信任所有
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            }).build();
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e1) {
+            e1.printStackTrace();
+        }
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
         HttpPost hp = new HttpPost(postUrl);
         CloseableHttpResponse response = null;
         String result = null;
@@ -33,9 +56,22 @@ public class APITools {
         }
         return result;
     }
-    
-    public String get(String getUrl){
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    public String get(String getUrl) {
+        SSLContext sslContext = null;
+        try {
+            sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+                // 信任所有
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            }).build();
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e1) {
+            e1.printStackTrace();
+        }
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
         HttpGet hp = new HttpGet(getUrl);
         CloseableHttpResponse response = null;
         String result = null;
